387ad32cf520f084e5468c6d70fed7d9384d739a
OlesiaMiller <52347583+OlesiaMiller@users.noreply.github.com>
2019-09-18 14:57:03 +0300

    Созданы функции для сортировки массива по цене и отзывам

---
+++ main.js
@@ -0,0 +1,116 @@
+var shop = [
+    {ratingRevievs: '264 отзыва',
+    price: {oldUan: '4 333 грн',
+            newUan: '3 799 грн'}, name: 'Motorola MOTO G4 (XT1622) Black'},
+    
+    {ratingRevievs: '1355 отзывов', price: '4 999 грн', name: 'Samsung Galaxy J7 J700H/DS Black + карта памяти 16гб + чехол + защитное стекло!'},
+    
+    {ratingRevievs:'426 отзывов', price: '5 199 грн', name: 'Samsung Galaxy J5 (2016) J510H/DS Black + защитное стекло + чехол!'},
+    {ratingRevievs: '403 отзыва', price: '4 349 грн', name: 'Xiaomi Redmi Note 4X 3/32GB Black'},
+    {ratingRevievs: '488 отзывов', price: '6 199 грн', name: 'Samsung Galaxy J7 (2016) J710F/DS Gold + защитное стекло + чехол!'},
+    {ratingRevievs: '198 отзывов', price: {oldUan: '3 495 грн', newUan: '2 995 грн'}, name: 'Lenovo K5 (A6020a40) Silver'}, {ratingRevievs: '352 отзыва', price: {oldUan: '9 799 грн', newUan: '7 999 грн'}, name: 'Apple iPhone 5s 16GB    Space Gray'},{ratingRevievs: '59 отзывов',price: '5 999 грн', name:    'Nokia 5 Dual Sim Tempered Blue'},
+    {ratingRevievs: '119 отзывов',price:    '11 999 грн', name: 'Samsung Galaxy A5 2017 Duos SM-A520 Black +    карта памяти 128гб!'},
+    {ratingRevievs: '1106 отзывов',price: '3 999 грн',    name: 'Samsung Galaxy J5 J500H/DS Black + чехол + защитное    стекло!'}, {ratingRevievs: '380 отзывов',price: '2 199 грн', name:    'Huawei Y3 II Tiffany (White-Blue) + чехол + защитное стекло!'},    {ratingRevievs: '86 отзывов',price: {oldUan: '24 999 грн', newUan: '22    999 грн'}, name: 'Samsung Galaxy S8 64GB Midnight Black + карта    памяти 64гб + оригинальный чехол!'},
+    {ratingRevievs: '177 отзывов',   price: '6 499 грн', name: 'Huawei P8 Lite 2017 White + УМБ Huawei    AP08Q + защитное стекло + чехол!'}, {ratingRevievs: '347 отзывов',   price: '4 299 грн', name: 'Xiaomi Redmi 4X 3/32GB Black    (Международная версия)'},
+    {ratingRevievs: '709 отзывов',price: '2    799 грн', name:' Samsung Galaxy J1 2016 SM-J120H Black + защитное    стекло + чехол!'},
+    {ratingRevievs: '527 отзывов',price: '3 999 грн', name:'Huawei Y6 Pro Gold + чехол + защитное стекло!'}, 
+    {ratingRevievs: '66 отзывов',price: '16 499 грн', name: 'Apple iPhone 6s 32GB Gold'},
+    {ratingRevievs: '14 отзывов',price: '11 499 грн', name: 'Apple iPhone 6 32GB Space Gray'}, {ratingRevievs: '70 отзывов',price: {oldUan: '7399 грн', newUan: '5 999 грн'}, name: 'Asus ZenFone 2 32GB (ZE551ML)    Black'},
+    {ratingRevievs: '45 отзывов',price:'4 299 грн', name: 'Nokia 3    Dual Sim Silver White + сертификаты 500 грн!'}, {ratingRevievs: '376    отзывов',price: '3 899 грн', name: 'Meizu M3 Note 32GB Grey    (Международная версия)'}, {ratingRevievs: '111 отзывов',price:    {oldUan: '9 999 грн', newUan: '7 999 грн'}, name: 'Sony Xperia X Dual    (F5122) White'}, 
+    {ratingRevievs: '40 отзывов',price: '2 222 грн', name:    'Lenovo Vibe C (A2020) Black + УМБ PowerPlant 5200 mAh в подарок!'}, {ratingRevievs: '93 отзыва', price: '18 999 грн', name: 'Apple iPhone 7    32GB Black'},
+    {ratingRevievs: '33 отзыва', price: '16 999 грн', name:    'Huawei P10 4/32GB Black + сертификат 2500грн + чехол Huawei Smart    View Cover!'},
+    {ratingRevievs: '71 отзыв',price: {oldUan: '2 399 грн',    newUan: '1 999 грн'}, name: 'LG K5 X220ds Gold'},
+    {ratingRevievs: '39    отзывов',price: '2 995 грн', name: 'Lenovo C2 Power (K10a40) Black'},
+    {ratingRevievs: '156 отзывов',price: '2 599 грн', name: 'Nous NS 5006    Gold'},
+    {ratingRevievs: '40 отзывов',price: '19 689 грн', name: 'LG G6    Mystic White'},
+    {ratingRevievs: '24 отзыва', price: '5 995 грн', name:    'Motorola MOTO G5 (XT1676) Grey'},
+    {ratingRevievs: '7 отзывов',price:    {oldUan: '10 999 грн', newUan: '9 999 грн'}, name: 'HTC One X10 Dual    Sim Silver'}, {ratingRevievs: '18 отзывов',price: {oldUan: '5 999 грн',    newUan: '4 999 грн'}, name: 'Sony Xperia L1 Dual Black'}
+];
+
+let div = document.createElement('div');
+div.className = "alert";
+div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";var list = document.querySelector('.output ul');
+
+
+ol.before('before'); // вставить строку "before" перед <ol>
+ol.after('after'); // вставить строку "after" после <ol>
+
+let liFirst = document.createElement('li');
+liFirst.innerHTML = 'prepend';
+ol.prepend(liFirst); // вставить liFirst в начало <ol>
+
+let liLast = document.createElement('li');
+liLast.innerHTML = 'append';
+ol.append(liLast); // вставить liLast в конец <ol>
+
+// var totalBox = document.querySelector('.output p');
+// var total = 0;
+// list.innerHTML = '';
+// totalBox.textContent = '';
+
+// for(var i = 0; i < shop.length; i++) {
+
+//     itemText = shop[i];
+   
+//     var listItem = document.createElement('li');
+//     listItem.textContent = itemText;
+//     list.appendChild(listItem);
+//    }    
+
+var reg = /\D+/g;
+function bubbleSort(arr) {
+    for (var i = 0, endI = arr.length - 1; i < endI; i++) {
+        var wasSwap = false;
+        for (var j = 0, endJ = endI - i; j < endJ; j++) {
+            if (+arr[j].ratingRevievs.replace(reg, '') > +arr[j + 1].ratingRevievs.replace(reg, '')) {
+                var swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+                wasSwap = true;
+            }
+        }
+        if (!wasSwap) break;
+    }
+    return arr;
+}
+
+
+
+
+
+function priceCompare(arr) {
+    for (var i = 0, endI = arr.length - 1; i < endI; i++) {
+        var wasSwap = false;
+        for (var j = 0, endJ = endI - i; j < endJ; j++) {
+
+            if (typeof arr[j].price['newUan'] == "string") {
+                arr[j].price = arr[j].price.newUan;
+
+            };  
+
+            if (typeof arr[j + 1].price['newUan'] == "string") {
+                arr[j + 1].price = arr[j + 1].price.newUan;
+
+            };  
+            
+
+            if (+arr[j].price.replace(reg, '') > +arr[j + 1].price.replace(reg, '')) {
+                var swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+                wasSwap = true;
+            }
+            };
+    
+        if (!wasSwap) break;
+    }
+    return arr;
+}
+
+// console.log(priceCompare(shop));
+
+
+
+
+
+              // }
